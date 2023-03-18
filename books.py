2282bb6c9f2c3f40227a2f9c8b40db0412242e2e
Claire Kim <clairekim@Claires-MBP.attlocal.net>
2023-03-17 19:17:02 -0700

    added books dictionary

---
+++ books.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+BOOKS = {
+    'book_1': {'title': 'Python for all', 'author':'Maria kyle','year': 2006},
+    'book_2': {'title': 'AI principles', 'author':'Daniel Walu','year': 2008},
+    'book_3': {'title': 'Scratch rocks', 'author':'claire kim','year': 2007},
+    'book_4': {'title': 'Sci for all', 'author':'Kaf Josh','year': 2009}
+
+}
+
+@app.put('/{book}')
+def update_book(bk_title:str, bk_author:str, book:str):
+    book = BOOKS[book]
+    book.title = bk_title
+    book.author = bk_author
+    return f'{book} has been updated'
+
+
+@app.get("/")
+async def home():
+    return BOOKS
+
+@app.get("/home")
+async def my_home():
+    return {"message": "Hello Iam home "}
+
+@app.get("/food")
+async def food():
+    return {"message": "Hello World of Food "}
