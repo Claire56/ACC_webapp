d440e5fac38b5276851e27ded3b5812e22f69bdc
Claire Kim <clairekim@Claires-MBP.attlocal.net>
2023-03-19 17:26:48 -0700

    query parameters

--- books.py
+++ books.py
@@ -1,32 +1,45 @@
-from fastapi import FastAPI
+from fastapi import FastAPI , Body
 
 app = FastAPI()
 
-BOOKS = {
-    'book_1': {'title': 'Python for all', 'author':'Maria kyle','year': 2006},
-    'book_2': {'title': 'AI principles', 'author':'Daniel Walu','year': 2008},
-    'book_3': {'title': 'Scratch rocks', 'author':'claire kim','year': 2007},
-    'book_4': {'title': 'Sci for all', 'author':'Kaf Josh','year': 2009},
-    'book_4': {'title': 'Added here', 'author':'Penny Miles','year': 2019}
-
-}
-
-@app.put('/{book}')
-def update_book(bk_title:str, bk_author:str, book:str):
-    book = BOOKS[book]
-    book.title = bk_title
-    book.author = bk_author
-    return f'{book} has been updated'
-
+BOOKS = [
+    {'title': 'Python for all', 'author':'Maria kyle','year': 2006},
+    {'title': 'AI principles', 'author':'Daniel Walu','year': 2008},
+    {'title': 'Scratch rocks', 'author':'claire kim','year': 2007},
+    {'title': 'Sci for all', 'author':'Kaf Josh','year': 2009},
+    {'title': 'Added here', 'author':'Penny Miles','year': 2019}
+]
 
 @app.get("/")
 async def home():
     return BOOKS
 
-@app.get("/home")
-async def my_home():
-    return {"message": "Hello Iam home "}
+@app.get("/books/{book_title}")
+async def get_one_book(book_title):
+    for book in BOOKS:
+        if book.title == book_title:
+            return book
 
-@app.get("/food")
-async def food():
-    return {"message": "Hello World of Food "}
+@app.post("/books/add_book")
+async def add_book(new_book = Body()):
+    BOOKS.append(new_book)
+
+
+@app.put('/books/update_book')
+def update_book(update_book = Body()):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get('title').casefold() == update_book.get('title').casefold() :
+            BOOKS[i] = update_book
+
+@app.delete('/books/delete_book/{book_title}')
+def delete_book(book_title:str):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get('title').casefold() == book_title.casefold() :
+            BOOKS.pop(i)
+            break
+
+    
+
+
+    
+
